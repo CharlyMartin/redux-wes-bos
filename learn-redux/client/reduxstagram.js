8d2f40b2bff7ee68209d809c381cf43e665e66d8
Charly MARTIN <charly.martin.1207@gmail.com>
2019-05-26 19:05:09 -0500

    Add simple react setup on entry file

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,9 @@
 // let's go!
+import React from "react";
+import ReactDOM from "react-dom";
+
+console.log("Hello from the console!");
+
+const rootElement = document.querySelector("#root")
+ReactDOM.render(<h1>Hello</h1>, rootElement);
+
